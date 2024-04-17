001a66bcb6e6d79929975d7e1690576c9e90dc15
trejder <2903392+trejder@users.noreply.github.com>
2024-04-17 17:20:49 +0200

    First version of dictionary-data framework

---
+++ js/data-map.js
@@ -0,0 +1,13 @@
+let map = {
+    human: {
+        polish: {
+            male: null,
+            female: human_polish_female
+        },
+    },
+    fantasy: {
+        albion: {
+            male: null
+        },
+    }
+};
---
+++ js/data/human-polish-female.js
@@ -0,0 +1,14 @@
+let human_polish_female = {
+    names: [
+        'Ada',
+        'Adela',
+        'Adolfina',
+        'Adrianna'
+    ],
+    surnames: [
+        'Abramowicz',
+        'Adamczak',
+        'Adamczewska',
+        'Adamczyk'
+    ]
+};
